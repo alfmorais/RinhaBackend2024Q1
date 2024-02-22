adaf38ae207b0451b755355bad0889f0023e73f3
alfmorais <alfredneto@hotmail.com>
2024-02-22 17:57:05 -0300

    Refactor: included name in customer database.

--- src/models/customers.py
+++ src/models/customers.py
@@ -5,16 +5,17 @@ commands = (
     """
     CREATE TABLE Clientes (
         id SERIAL PRIMARY KEY,
+        nome VARCHAR(64),
         limite INTEGER,
         saldo_inicial INTEGER DEFAULT 0
     );
     """,
     """
-    INSERT INTO Clientes (limite, saldo_inicial) VALUES
-        (100000, 0),
-        (80000, 0),
-        (1000000, 0),
-        (10000000, 0),
-        (500000, 0);
+    INSERT INTO Clientes (nome, limite, saldo_inicial) VALUES
+        ('o barato sai caro', 1000 * 100, 0),
+        ('zan corp ltda', 800 * 100, 0),
+        ('les cruders', 10000 * 100, 0),
+        ('padaria joia de cocaia', 100000 * 100, 0),
+        ('kid mais', 5000 * 100, 0);
     """,
 )
